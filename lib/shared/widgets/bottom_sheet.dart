4ad9c135f6020b9b7bcc7d33bfdcf93be6ae8974
LuanRoger <luan.roger.2003@gmail.com>
2023-01-19 10:49:30 -0300

    refactor: create widget for `BottomSheet`
    - Create information button on `bottom_left_card`

---
+++ lib/shared/widgets/bottom_sheet.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class BottomSheet {
+  final BuildContext context;
+  Widget body;
+  double? height;
+
+  BottomSheet(this.context, {required this.body, this.height});
+
+  void show() {
+    showModalBottomSheet(
+        context: context,
+        useSafeArea: true,
+        builder: (context) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(height: height, child: body),
+          );
+        });
+  }
+}
--- lib/shared/widgets/cards/bottom_left_card.dart
+++ lib/shared/widgets/cards/bottom_left_card.dart
@@ -6,12 +6,13 @@ import 'package:cupon_take/models/user_info.dart';
 import 'package:cupon_take/providers/providers.dart';
 import 'package:cupon_take/services/cupon_services.dart';
 import 'package:cupon_take/shared/responsive_breakpoints_name.dart';
+import 'package:cupon_take/shared/widgets/bottom_sheet.dart';
 import 'package:cupon_take/shared/widgets/cards/cards_base.dart';
 import 'package:cupon_take/shared/widgets/dynamic_ex_fab.dart';
 import 'package:cupon_take/shared/widgets/last_redeem_info_card.dart';
 import 'package:cupon_take/shared/widgets/no_account_message.dart';
 import 'package:cupon_take/shared/widgets/user_info_chip.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide BottomSheet;
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:responsive_framework/responsive_framework.dart';
@@ -22,7 +23,27 @@ class BottomLeftCard extends CardBase {
   BottomLeftCard(this.userInfo, {super.key, required super.cardTitle});
 
   @override
-  Widget? headerActions(BuildContext context) => null;
+  Widget? headerActions(BuildContext context) {
+    return Row(children: [
+      IconButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                      title: Text("Informações"),
+                      content: Text(
+                          "- Os cupons resgatados podem, posteriormente, serem vistos no histórico.\n" +
+                              "- Para resgatar um cupom é nescessario ter 50 pontos na conta."),
+                      actions: [
+                        TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: const Text("Continuar"))
+                      ],
+                    ));
+          },
+          icon: const Icon(Icons.info_outline_rounded))
+    ]);
+  }
 
   Future<String?> _redeemCupon(String authKey) async {
     CuponServices cuponServices = CuponServices();
@@ -36,6 +57,7 @@ class BottomLeftCard extends CardBase {
   Widget virtualBuild(BuildContext context, WidgetRef ref) {
     final authKey = ref.read(userAuthProvider);
     final userInfo = ref.watch(fetchUserInfoProvider);
+
     final isLoadingState = useState(false);
     final lastCuponRedeemState = useState<String?>(null);
 
